8110a0ab042fd68d9ec7279d9b55fe44074bc5c9
satells <msergiost@hotmail.com>
2021-02-09 11:08:15 -0300

    config servlet hello

---
+++ 1JavaServletTutorialForBeginners/src/main/java/org/o7planning/tutorial/servlet/HelloServlet.java
@@ -0,0 +1,9 @@
+package org.o7planning.tutorial.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+}
